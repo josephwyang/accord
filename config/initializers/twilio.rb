2c905e5ebc466582ea15fe6a750eb47caaffd944
joseph <josephwyang@gmail.com>
2021-11-08 18:57:50 -0500

    test twilio error

--- config/initializers/twilio.rb
+++ config/initializers/twilio.rb
@@ -1,4 +1,4 @@
 Twilio.configure do |config|
-  config.account_sid = Rails.env == "development" ? Rails.application.credentials.twilio[:account_sid] : ENV["ACCOUNT_SID"]
-  config.auth_token = Rails.env == "development" ? Rails.application.credentials.twilio[:auth_token] : ENV["AUTH_TOKEN"]
+  # config.account_sid = Rails.env == "development" ? Rails.application.credentials.twilio[:account_sid] : ENV["ACCOUNT_SID"]
+  # config.auth_token = Rails.env == "development" ? Rails.application.credentials.twilio[:auth_token] : ENV["AUTH_TOKEN"]
 end
